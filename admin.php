dc81017965c74c950238f1745b6e580c1743b5ab
BSaz <b-sazonov@list.ru>
2017-08-31 23:24:56 +0300

    new files

---
+++ admin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+<form action="test.php" method="post">
+    <div>
+        <p>Сколько будет 2 + 2?</p>
+        <label><input name="q1" type="radio" value="a">4</label>
+        <label><input name="q1" type="radio" value="b">2</label>
+        <label><input name="q1" type="radio" value="c">1</label>
+    </div>
+    <div>
+        <p>Сколько будет 5 - 2?</p>
+        <label><input name="q2" type="radio" value="a">3</label>
+        <label><input name="q2" type="radio" value="b">2</label>
+        <label><input name="q2" type="radio" value="c">1</label>
+    </div>
+    <button type="submit">Результат</button>
+</form>
+</body>
+</html>
